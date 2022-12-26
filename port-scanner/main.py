e4acf46267076baf7013493b422f63ab05b62129
shivam-luthra <shivamluthranx@gmail.com>
2022-12-26 06:14:17 -0800

    limit concurrent connections

--- port-scanner/main.py
+++ port-scanner/main.py
@@ -3,39 +3,60 @@ import asyncio
 '''
 coroutine to scan a particular host's port - timeout of 3 seconds provided.
 '''
-async def port_scanner(host, port, timeout=5):                                                    
+async def port_scanner(host, port, timeout=5):
     coro = asyncio.open_connection(host, port)                                                      
     try:
         _, writer = await asyncio.wait_for(coro, timeout=timeout)
         writer.close()
         return True, port
     except asyncio.TimeoutError:
+        
         return False, port
 
+async def scanner(host, queue):
+    while True:
+        port = await queue.get()
+        if not port:
+            await queue.put(port)
+            break
+        if await port_scanner(host, port):
+            print(f"{host}:{port} [OPEN]")
+        queue.task_done()
 '''
 Main coroutine that will call port_scanner coroutine
 ports - iterable (range object) of ports to scan
 '''
-async def main(host, ports):
+async def main(host, ports, limit=500):
     print(f"scanning {host}")
-    coros = [port_scanner(host, port) for port in ports]
-    '''
-    When scanning an entire port range, we will hit the problem where we cross the limit of number of files opened in out OS (check ulimit -n).
-    You can either increase this limit, or process these coroutines in batches. Either option works.
-    Batching will be a little slower but will take care of larger number of ports that can't be taken care of in one shot.
-    '''
-    # asyncio.gather for getting all results before proceeding
-    # results = await asyncio.gather(*coros)
-    # for port, result in zip(ports, results):
+
+    task_queue = asyncio.Queue()
+
+    workers = [asyncio.create_task(scanner(host, task_queue)) for _ in range(limit)]
+    for port in ports:
+        await task_queue.put(port)
+    
+    await task_queue.join()
+
+    await task_queue.put(None)
+    # coros = [port_scanner(host, port) for port in ports]
+    # '''
+    # When scanning an entire port range, we will hit the problem where we cross the limit of number of files opened in out OS (check ulimit -n).
+    # You can either increase this limit, or process these coroutines in batches. Either option works.
+    # Batching will be a little slower but will take care of larger number of ports that can't be taken care of in one shot.
+    # OR, You can use an asyncio.Queue and limit the number of sockets being opened.
+    # '''
+    # # asyncio.gather for getting all results before proceeding
+    # # results = await asyncio.gather(*coros)
+    # # for port, result in zip(ports, results):
+    # #     if result:
+    # #         print(f"{host}:{port} [OPEN]")
+
+    # # asyncio.as_completed for reacting to coroutines as and when they're completed
+    # # here timeout is not necessary
+    # for coro in asyncio.as_completed(coros):
+    #     result, port = await coro
     #     if result:
     #         print(f"{host}:{port} [OPEN]")
-
-    # asyncio.as_completed for reacting to coroutines as and when they're completed
-    # here timeout is not necessary
-    for coro in asyncio.as_completed(coros):
-        result, port = await coro
-        if result:
-            print(f"{host}:{port} [OPEN]")
 
 host = "python.org"
 ports = range(400,500)
