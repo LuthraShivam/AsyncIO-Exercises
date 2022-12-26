e8d70a51eeba9a0aac7838f46cc681b72829df1c
shivam-luthra <shivamluthranx@gmail.com>
2022-12-26 05:27:35 -0800

    respond to coroutine result as and when it's received

--- port-scanner/main.py
+++ port-scanner/main.py
@@ -5,12 +5,12 @@ coroutine to scan a particular host's port - timeout of 3 seconds provided.
 '''
 async def port_scanner(host, port, timeout=5):                                                    
     coro = asyncio.open_connection(host, port)                                                      
-    try:                                                      
+    try:
         _, writer = await asyncio.wait_for(coro, timeout=timeout)
-        writer.close()                                   
-        return True                                                                                 
-    except asyncio.TimeoutError:                                                                    
-        return False
+        writer.close()
+        return True, port
+    except asyncio.TimeoutError:
+        return False, port
 
 '''
 Main coroutine that will call port_scanner coroutine
@@ -24,8 +24,16 @@ async def main(host, ports):
     You can either increase this limit, or process these coroutines in batches. Either option works.
     Batching will be a little slower but will take care of larger number of ports that can't be taken care of in one shot.
     '''
-    results = await asyncio.gather(*coros)
-    for port, result in zip(ports, results):
+    # asyncio.gather for getting all results before proceeding
+    # results = await asyncio.gather(*coros)
+    # for port, result in zip(ports, results):
+    #     if result:
+    #         print(f"{host}:{port} [OPEN]")
+
+    # asyncio.as_completed for reacting to coroutines as and when they're completed
+    # here timeout is not necessary
+    for coro in asyncio.as_completed(coros):
+        result, port = await coro
         if result:
             print(f"{host}:{port} [OPEN]")
 
